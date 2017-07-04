6ba6ebeb29f1997ab069321e6211ee70ee3015b6
Eito Katagiri <eitoball@gmail.com>
2017-07-05 01:13:28 +0900

    [ch3] DRYer specs with describe, context, before and after

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -18,10 +18,14 @@ describe Contact do
   it 'is invalid without a lastname' do
     contact = Contact.new(lastname: nil)
     contact.valid?
-    expect(contact.errors[:lastname]).to invalid('can\'t be blank')
+    expect(contact.errors[:lastname]).to include('can\'t be blank')
   end
 
-  it 'is invalid without an email address'
+  it 'is invalid without an email address' do
+    contact = Contact.new(email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include('can\'t be blank')
+  end
 
   it 'is invalid with a duplicate email address' do
     Contact.create(
@@ -42,41 +46,35 @@ describe Contact do
     expect(contact.name).to eq 'John Doe'
   end
 
-  it 'returns a sorted array of results that match' do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-    expect(Contact.by_letter('J')).to eq [johnson, jones]
-  end
+  describe 'filter last name by letter' do
+    before :each do
+      @smith = Contact.create(
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'jsmith@example.com'
+      )
+      @jones = Contact.create(
+        firstname: 'Tim',
+        lastname: 'Jones',
+        email: 'tjones@example.com'
+      )
+      @johnson = Contact.create(
+        firstname: 'John',
+        lastname: 'Johnson',
+        email: 'jjohnson@example.com'
+      )
+    end
 
-  it 'omits results that do not match' do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-    expect(Contact.by_letter('J')).not_to include smith
+    context 'matching letters' do
+      it 'returns a sorted array of results that match' do
+        expect(Contact.by_letter('J')).to eq [@johnson, @jones]
+      end
+    end
+
+    context 'non-matching letters' do
+      it 'omits results that do not match' do
+        expect(Contact.by_letter('J')).not_to include @smith
+      end
+    end
   end
 end
