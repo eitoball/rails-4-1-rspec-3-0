d8d7e206eea418518da7d55c6f6dcb68eb966d60
Eito Katagiri <eitoball@gmail.com>
2017-07-05 01:00:00 +0900

    [ch3] Testing validations

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -9,9 +9,32 @@ describe Contact do
     expect(contact).to be_valid
   end
 
-  it 'is invalid without a firstname'
-  it 'is invalid without a lastname'
+  it 'is invalid without a firstname' do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include('can\'t be blank')
+  end
+
+  it 'is invalid without a lastname' do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to invalid('can\'t be blank')
+  end
+
   it 'is invalid without an email address'
-  it 'is invalid with a duplicate email address'
+
+  it 'is invalid with a duplicate email address' do
+    Contact.create(
+      firstname: 'Joe', lastname: 'Tester',
+      email: 'tester@example.com'
+    )
+    contact = Contact.new(
+      firstname: 'Jane', lastname: 'Tester',
+      email: 'tester@example.com'
+    )
+    contact.valid?
+    expect(contact.errors[:email]).to include('has already been taken')
+  end
+
   it 'returns a contact\'s full name as a string'
 end
