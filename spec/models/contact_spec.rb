692b074ca6ae3c479bcfb7613139c598aac6f7af
Eito Katagiri <eitoball@gmail.com>
2017-07-05 01:33:58 +0900

    [ch4] Adding factories to the application

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Contact do
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:contact)).to be_valid
+  end
+
   it 'is valid with a firstname, lastname and email' do
     contact = Contact.new(
       firstname: 'Aaron',
@@ -10,32 +14,34 @@ describe Contact do
   end
 
   it 'is invalid without a firstname' do
-    contact = Contact.new(firstname: nil)
+    contact = FactoryGirl.build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include('can\'t be blank')
   end
 
   it 'is invalid without a lastname' do
-    contact = Contact.new(lastname: nil)
+    contact = FactoryGirl.build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include('can\'t be blank')
   end
 
   it 'is invalid without an email address' do
-    contact = Contact.new(email: nil)
+    contact = FactoryGirl.build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include('can\'t be blank')
   end
 
+  it 'returns a contact\'s full name as a string' do
+    contact = FactoryGirl.build(:contact,
+                                firstname: 'Jane',
+                                lastname: 'Smith'
+                               )
+    expect(contact.name).to eq 'Jane Smith'
+  end
+
   it 'is invalid with a duplicate email address' do
-    Contact.create(
-      firstname: 'Joe', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
-    contact = Contact.new(
-      firstname: 'Jane', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
+    FactoryGirl.create(:contact, email: 'aaron@example.com')
+    contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
     contact.valid?
     expect(contact.errors[:email]).to include('has already been taken')
   end
